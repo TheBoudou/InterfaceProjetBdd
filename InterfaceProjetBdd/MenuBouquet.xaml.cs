efa40249d5bfa4bda042bc5c6f30e62cd1a37730
TheBoudou <tom.huchet@edu.devinci.fr>
2023-05-12 20:26:00 +0200

    l

--- InterfaceProjetBdd/MenuBouquet.xaml.cs
+++ InterfaceProjetBdd/MenuBouquet.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -31,6 +33,54 @@ namespace InterfaceProjetBdd
             var Menu = new MenuPrincipal(connectionstring);
             Menu.Show();
             this.Close();
+        }
+        public void FillGrid1(string connectionstring)
+        {
+            MySqlConnection connection = new MySqlConnection(connectionstring);
+            connection.Open();
+            string commande = "Select * from bouquet_std ;";
+            MySqlCommand cmdSel = new MySqlCommand(commande, connection);
+            DataTable dt = new DataTable();
+            MySqlDataAdapter da = new MySqlDataAdapter(cmdSel);
+            try
+            {
+                da.Fill(dt);
+                dataGrid1.ItemsSource = dt.DefaultView;
+            }
+            catch (Exception ex)
+            {
+
+            }
+            connection.Close();
+        }
+        public void FillGrid2(string connectionstring)
+        {
+            MySqlConnection connection = new MySqlConnection(connectionstring);
+            connection.Open();
+            string commande = "Select * from bouquet_perso ;";
+            MySqlCommand cmdSel = new MySqlCommand(commande, connection);
+            DataTable dt = new DataTable();
+            MySqlDataAdapter da = new MySqlDataAdapter(cmdSel);
+            try
+            {
+                da.Fill(dt);
+                dataGrid2.ItemsSource = dt.DefaultView;
+            }
+            catch (Exception ex)
+            {
+
+            }
+            connection.Close();
+        }
+
+        private void dataGrid1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void dataGrid2_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
 }
